b88f22e72603c56da89859d5883fd928c7d3fd0c
BenRodgers <benjaminarodgers@gmail.com>
2019-01-20 14:45:27 +1000

    Added comments and extra check for already chosen cards

--- game.c
+++ game.c
@@ -29,9 +29,11 @@ typedef struct Game Game;
 */
 void validate_deck_file(Game* game, int argc, char** argv){
     char buffer[80];        
+    //Read deck file
     FILE* fp = fopen(argv[1], "r");
     memset(buffer, 0, sizeof(buffer)); 
-    
+
+    //Check each line correct contents     
     while(fgets(buffer, sizeof(buffer), fp) != NULL){
         for(int i = 0; i < strlen(buffer); i++) {
             if(!isdigit(buffer[i]) && buffer[i] != 'K' && buffer[i] != 'A'
@@ -53,12 +55,16 @@ void validate_deck_file(Game* game, int argc, char** argv){
 void make_face_up(Game* game, int argc, char** argv){
     char buffer[80];
     FILE* fp = fopen(argv[1], "r");
+
+    //Allocate rows
     game->faceUp = (char**)malloc(4 * sizeof(char*));
     for(int i=0; i<4; i++){
+        //Allocate column for each row
         game->faceUp[i] = (char*) malloc(13 * sizeof(char));
     }
     
     int i = 0;
+    //Place X for different board positions 
     while(fgets(buffer, sizeof(buffer), fp) != NULL){
         for(int j = 0; j < 13; j++){
             game->faceUp[i][j] = 'X';
@@ -75,7 +81,9 @@ void make_face_up(Game* game, int argc, char** argv){
 *
 */
 void print_face_up(Game* game) {    
+    //Print row
     for(int i=0; i<4; i++){
+        //Print each column
         for(int j=0; j<13; j++){
             printf("%c ", game->faceUp[i][j]);
         }
@@ -84,21 +92,23 @@ void print_face_up(Game* game) {
 }
 
 /**
-*   Function for validating the deck file
+*   Function for storing the deck file (face-down)
 *   
 *
 */
 void store_deck_file(Game* game, int argc, char** argv){
     char buffer[80];
+    //Read deck file
     FILE* fp = fopen(argv[1], "r");
-
+    //Allocate each row
     game->board = (char**)malloc(4 * sizeof(char*));
-    
+    //Allocate each column
     for(int i=0; i<4; i++){
         game->board[i] = (char*) malloc(13 * sizeof(char));
     }
     
     int i = 0;
+    //Read deck file contents into memory (row/column)
     while(fgets(buffer, sizeof(buffer), fp) != NULL){
         for(int j = 0; j < 13; j++){
             game->board[i][j] = buffer[j];
@@ -108,22 +118,6 @@ void store_deck_file(Game* game, int argc, char** argv){
     fclose(fp);
 }
 
-
-/**
-*   Function for printing the board
-*
-*
-*/
-void print_board(Game* game){
-    for(int i=0; i<4; i++){
-        for(int j=0; j<13; j++){
-            printf("%c", game->board[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-
 /**
 *   Function for checking if a move is valid
 *
@@ -132,27 +126,34 @@ void print_board(Game* game){
 int is_move_valid(Game* game, char* moveOne, char* moveTwo){
     int moveOneX, moveOneY, moveTwoX, moveTwoY;
     char moveOneR, moveTwoR, moveOneC, moveTwoC;
-
+    //Check valid input for first move
     if(sscanf(moveOne, "%c%d%c%d", &moveOneR, &moveOneY, &moveOneC, &moveOneX) != 4){
         return 0;
     }
-
+    //Check valid input for second move
     if(sscanf(moveTwo, "%c%d%c%d", &moveTwoR, &moveTwoY, &moveTwoC, &moveTwoX) != 4){
         return 0;
     }
-
+    //Check r and c in the answer
     if(moveOneR != 'r' || moveOneC != 'c' || moveTwoR != 'r' || moveOneC != 'c'){
         return 0;
     }
 
+    //Check if valid column has been chosen
     if(moveOneX < 0 || moveOneX > 12 || moveTwoX < 0 || moveTwoX > 12){
         return 0;
     }
-
+    //Check if valid row has been chosen
     if(moveOneY < 0 || moveOneY > 3 || moveTwoY < 0 || moveTwoY > 12) {
         return 0;
     }
+    
+    //Check cards haven't already been chosen
+    if(game->faceUp[moveOneY][moveOneX] == '-' || game->faceUp[moveTwoY][moveTwoX] == '-'){
+        return 0;
+    }
 
+    //Successful move
     return 1;
 }
 
@@ -164,17 +165,20 @@ int is_move_valid(Game* game, char* moveOne, char* moveTwo){
 */
 void print_temporary_board(Game* game, int moveOneX, int moveOneY, 
     int moveTwoX, int moveTwoY) {
-    
+    //Print the two cards chosen by the players, print 'X' otherwise
     for(int i=0; i<4; i++){
         for(int j=0; j<13; j++) {
+            //Show first card chosen  
             if(i == moveOneY && j == moveOneX){
                 printf("%c ", game->board[moveOneY][moveOneX]);
                 continue;
             }
+            //Show second card chosen
             if(i == moveTwoY && j == moveTwoX){
                 printf("%c ", game->board[moveTwoY][moveTwoX]);
                 continue;
             }
+            //Print X
             else {
                 printf("%c ", game->faceUp[i][j]);
             }
@@ -206,6 +210,7 @@ void print_current_scores(Game* game){
 *
 */
 void update_score(Game* game){
+    //Add points
     if(game->currentPlayer == 0) {
         game->playerZeroPoints++;
     }
@@ -223,27 +228,33 @@ void update_score(Game* game){
 */
 void choose_card(Game* game, char* moveOne, char* moveTwo) {
    int moveOneY, moveOneX;
-   //Get coordinates of card
+   //Get coordinates of each card
    sscanf(moveOne, "r%dc%d", &moveOneY, &moveOneX);
    
    int moveTwoY, moveTwoX;
    sscanf(moveTwo, "r%dc%d", &moveTwoY, &moveTwoX);
-
+   
+   //Successful pair chosen
    if(game->board[moveOneY][moveOneX] == game->board[moveTwoY][moveTwoX]){
-        
         printf("MATCH!\n");
-        
+        //Replace 'X' with '-' to show these cards have been taken     
         game->faceUp[moveOneY][moveOneX] = '-';
         game->faceUp[moveTwoY][moveTwoX] = '-';
-        print_face_up(game);        
+        //Print face up board
+        print_face_up(game); 
+        //Add point       
         update_score(game);
+        //Print scores
         print_current_scores(game);
             
         
     }
+    //Pair not found
     else {
         printf("NO MATCH!\n");
+        //Show clues to the player
         print_temporary_board(game, moveOneX, moveOneY, moveTwoX, moveTwoY);
+        //Show normal board without clues
         print_face_up(game);
     }
 }
@@ -289,6 +300,7 @@ void check_arguments(int argc, char** argv){
 *
 */
 int game_not_over(Game* game){
+    //Check there is still a pair on the board
     for(int i=0; i<4; i++){
         for(int j=0; j<12; j++){
             if(game->faceUp[i][j] == 'X'){
@@ -312,21 +324,25 @@ void play_game(Game* game){
         printf("Player %d turn: ", game->currentPlayer);
         char buffer[80];
         memset(buffer, 0, sizeof(buffer));
+        //Get player input
         fgets(buffer, sizeof(buffer), stdin);
         char firstMove[20], secondMove[20];
         memset(firstMove, 0, sizeof(firstMove));
         memset(secondMove, 0, sizeof(secondMove));
         
-
+        //Check two moves entered
         if(sscanf(buffer, "%s %s", firstMove, secondMove) != 2){
             continue;
         }    
-        
+        //Check moves are valid
         if(!is_move_valid(game, firstMove, secondMove)){
             continue;
         }
+        //Valid cards chosen
         else {
+            //Select the card for checking
             choose_card(game, firstMove, secondMove);
+            //Update the player
             game->currentPlayer++;
             game->currentPlayer%=2;
         }
