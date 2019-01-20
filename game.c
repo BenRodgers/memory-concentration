3c95a126974eaaf99b6d73defc33d0bd5d219eb0
BenRodgers <benjaminarodgers@gmail.com>
2019-01-20 14:21:58 +1000

    Added comments and checking for invalid input

--- game.c
+++ game.c
@@ -123,27 +123,42 @@ void print_board(Game* game){
     }
 }
 
+
+/**
+*   Function for checking if a move is valid
+*
+*
+*/
 int is_move_valid(Game* game, char* moveOne, char* moveTwo){
-    if(moveOne[0]!= 'r' || moveTwo[0] != 'r'){
-        return 0;
-    }
-    
-    if(moveOne[2]!= 'c' || moveTwo[2] != 'c'){
+    int moveOneX, moveOneY, moveTwoX, moveTwoY;
+    char moveOneR, moveTwoR, moveOneC, moveTwoC;
+
+    if(sscanf(moveOne, "%c%d%c%d", &moveOneR, &moveOneY, &moveOneC, &moveOneX) != 4){
         return 0;
     }
 
-    if(moveOne[1]>3 || moveTwo[1]>3){
+    if(sscanf(moveTwo, "%c%d%c%d", &moveTwoR, &moveTwoY, &moveTwoC, &moveTwoX) != 4){
         return 0;
-    }   
-    
- 
-    return 0;
+    }
+
+    if(moveOneR != 'r' || moveOneC != 'c' || moveTwoR != 'r' || moveOneC != 'c'){
+        return 0;
+    }
+
+    if(moveOneX < 0 || moveOneX > 12 || moveTwoX < 0 || moveTwoX > 12){
+        return 0;
+    }
+
+    if(moveOneY < 0 || moveOneY > 3 || moveTwoY < 0 || moveTwoY > 12) {
+        return 0;
+    }
+
+    return 1;
 }
 
 
 /*
 *   Function for displaying the board and revealing the selected cards
-*
 *
 *
 */
@@ -166,21 +181,27 @@ void print_temporary_board(Game* game, int moveOneX, int moveOneY,
         }
         printf("\n");
     }
+    //Show the board for three seconds
     sleep(3);
+    //Clear the terminal screen
     system("clear");
 } 
 
-void print_current_scores(Game* game){
-
-    printf("Scores:\n");
-    
-    printf("Player 0: %d\n", game->playerZeroPoints);
-    printf("Player 1: %d\n", game->playerOnePoints);
-
-}
 
 /**
+*   Function for printing the current scores
 *
+*
+*
+*/
+void print_current_scores(Game* game){
+    printf("Scores:\n");    
+    printf("Player 0: %d\n", game->playerZeroPoints);
+    printf("Player 1: %d\n", game->playerOnePoints);
+}
+
+/** 
+*   Function for updating the score
 *
 *
 */
@@ -201,23 +222,21 @@ void update_score(Game* game){
 *
 */
 void choose_card(Game* game, char* moveOne, char* moveTwo) {
-    int moveOneY, moveOneX;
-    //Get coordinates of card
+   int moveOneY, moveOneX;
+   //Get coordinates of card
    sscanf(moveOne, "r%dc%d", &moveOneY, &moveOneX);
    
-    int moveTwoY, moveTwoX;
+   int moveTwoY, moveTwoX;
+   sscanf(moveTwo, "r%dc%d", &moveTwoY, &moveTwoX);
 
-    sscanf(moveTwo, "r%dc%d", &moveTwoY, &moveTwoX);
-
-    if(game->board[moveOneY][moveOneX] == game->board[moveTwoY][moveTwoX]){
+   if(game->board[moveOneY][moveOneX] == game->board[moveTwoY][moveTwoX]){
         
         printf("MATCH!\n");
-
+        
         game->faceUp[moveOneY][moveOneX] = '-';
         game->faceUp[moveTwoY][moveTwoX] = '-';
-    
-        print_face_up(game);
-        
+        print_face_up(game);        
+        update_score(game);
         print_current_scores(game);
             
         
@@ -227,7 +246,6 @@ void choose_card(Game* game, char* moveOne, char* moveTwo) {
         print_temporary_board(game, moveOneX, moveOneY, moveTwoX, moveTwoY);
         print_face_up(game);
     }
-
 }
 
 
@@ -238,25 +256,25 @@ void choose_card(Game* game, char* moveOne, char* moveTwo) {
 *
 */
 void check_arguments(int argc, char** argv){
-
+    //Check correct arg count
     if(argc != 4){
         fprintf(stderr, "Usage: memory deckfile p1type p2type\n");
         fflush(stderr);
         exit(1);
     }
-    
+    //Check deck file is available
     if(access(argv[1], F_OK)== -1){
         fprintf(stderr, "Can't access deck file\n");
         fflush(stderr);
         exit(2);
     }
-    
+    //Check valid player type
     if(argv[2][0]!= 'h' && argv[3][0]!='p'){
         fprintf(stderr, "Invalid player type\n");
         fflush(stderr);
         exit(4);
     }
-    
+    //Check valid player type - no extra chars
     if(argv[2][1]!= '\0' && argv[2][1]!='\0'){
         fprintf(stderr, "Invalid player type\n");
         fflush(stderr);
@@ -264,8 +282,13 @@ void check_arguments(int argc, char** argv){
     }
 }
 
+/**
+*   Function for checking if the game is over
+*
+*
+*
+*/
 int game_not_over(Game* game){
-
     for(int i=0; i<4; i++){
         for(int j=0; j<12; j++){
             if(game->faceUp[i][j] == 'X'){
@@ -278,31 +301,47 @@ int game_not_over(Game* game){
 }
 
 /**
-*
+*   Function for the main game loop
 *
 *
 */
 void play_game(Game* game){
     
     while(game_not_over(game)){
+        //Prompt player
         printf("Player %d turn: ", game->currentPlayer);
         char buffer[80];
         memset(buffer, 0, sizeof(buffer));
         fgets(buffer, sizeof(buffer), stdin);
-        char firstWord[20], secondWord[20];
-        sscanf(buffer, "%s %s", firstWord, secondWord);    
-        choose_card(game, firstWord, secondWord);
-        game->currentPlayer++;
-        game->currentPlayer%=2;
-
+        char firstMove[20], secondMove[20];
+        memset(firstMove, 0, sizeof(firstMove));
+        memset(secondMove, 0, sizeof(secondMove));
         
+
+        if(sscanf(buffer, "%s %s", firstMove, secondMove) != 2){
+            continue;
+        }    
+        
+        if(!is_move_valid(game, firstMove, secondMove)){
+            continue;
+        }
+        else {
+            choose_card(game, firstMove, secondMove);
+            game->currentPlayer++;
+            game->currentPlayer%=2;
+        }
     }
 
 }
 
+/**
+*   Function for initializing the game variables to zero
+*
+*
+*
+*/
 void initialize_game(Game* game){
     game->currentPlayer = 0;
-
     game->playerZeroPoints = 0;
     game->playerOnePoints = 0;
 }
@@ -313,15 +352,21 @@ void initialize_game(Game* game){
 *
 */
 int main(int argc, char** argv){
-    check_arguments(argc, argv);   
+    //Validate game arguments
+    check_arguments(argc, argv);
+    //Make game    
     Game* game = malloc(sizeof(Game));
+    //Check deck file
     validate_deck_file(game, argc, argv);
+    //Save deck file into memory
     store_deck_file(game, argc, argv);
+    //Make cards visible to players
     make_face_up(game, argc, argv);
+    //Setup game variables 
     initialize_game(game);
-
+    //Print cards for displaying to player
     print_face_up(game);
-    
+    //Play game
     play_game(game);   
 
     return 0;
